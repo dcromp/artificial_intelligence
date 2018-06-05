73e5582a4076db11b4d834fa20fb0991d061800f
David Crompton <dcromp@Davids-iMac.local>
2018-06-05 20:21:08 +0200

    controller

--- CarND-Capstone/ros/src/twist_controller/dbw_node.py
+++ CarND-Capstone/ros/src/twist_controller/dbw_node.py
@@ -55,28 +55,29 @@ class DBWNode(object):
 
         # TODO: Create `Controller` object
         # self.controller = Controller(<Arguments you wish to provide>)
-        self.controller = Controller( vehicle_mass = vehicle_mass,
-                                    fuel_capacity = fuel_capacity,
-                                    brake_deadband = brake_deadband,
-                                    decel_limit = decel_limit,
-                                    accel_limit = accel_limit,
-                                    wheel_radius = wheel_radius,
-                                    wheel_base = wheel_base,
-                                    steer_ratio = steer_ratio,
-                                    max_lat_accel = max_lat_accel,
-                                    max_steer_angle = max_steer_angle
-                                    )
+        self.controller = Controller(
+            vehicle_mass=vehicle_mass,
+            fuel_capacity=fuel_capacity,
+            brake_deadband=brake_deadband,
+            decel_limit=decel_limit,
+            accel_limit=accel_limit,
+            wheel_radius=wheel_radius,
+            wheel_base=wheel_base,
+            steer_ratio=steer_ratio,
+            max_lat_accel=max_lat_accel,
+            max_steer_angle=max_steer_angle
+        )
 
         # TODO: Subscribe to all the topics you need to
 
-        rospy.Subscriber('/dbw_enabled', Bool, self.dbw_enabled_cb)
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)
         rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cb)
         rospy.Subscriber('/current_velocity', TwistStamped, self.velocity_cb)
 
         self.current_vel = None
         self.linear_vel = None
         self.angular_vel = None
-        self.dbw_enabled = None  ### CHANGE ME BACK ###
+        self.dbw_enabled = True  ### CHANGE ME BACK ###
         self.curr_ang_vel = None
         self.throttle = self.brake = self.steer = 0
 
